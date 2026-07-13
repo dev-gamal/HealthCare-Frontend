ee4c0297124c1f4f662add44dee55cee88f6d444
Gamal Badie <149597937+bd-gamal@users.noreply.github.com>
2026-07-13 15:35:24 +0100

    feat: implement routing with React Router in App component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Patients from "./pages/Patients";
+import Doctors from "./pages/Doctors";
+import Appointments from "./pages/Appointments";
+import Files from "./pages/Files";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/patients" element={<Patients />}></Route>
+      <Route path="/doctors" element={<Doctors />}></Route>
+      <Route path="/appointments" element={<Appointments />}></Route>
+      <Route path="/files" element={<Files />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
